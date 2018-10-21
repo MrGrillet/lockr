9223a87d85c710b895538587202df3c2bf9f2f08
Peter <pagrillet@gmail.com>
2018-10-21 14:38:54 +0100

    Adding the theme and assets

---
+++ app/assets/javascripts/application_body.js
@@ -0,0 +1,18 @@
+//= require_self
+
+//= require ./body/app
+//= require ./body/flickity.min
+//= require ./body/easypiechart.min
+//= require ./body/parallax
+//= require ./body/typed.min
+//= require ./body/datepicker
+//= require ./body/isotope.min
+//= require ./body/ytplayer.min
+//= require ./body/lightbox.min
+//= require ./body/granim.min
+//= require ./body/jquery.steps.min
+//= require ./body/countdown.min
+//= require ./body/twitterfetcher.min
+//= require ./body/spectragram.min
+//= require ./body/smooth-scroll.min
+//= require ./body/scripts
